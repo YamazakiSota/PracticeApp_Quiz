0f5a5e529ec4428ce1bfb4804f63db25931a973b
YamazakiSota <so-chan.y@ezweb.ne.jp>
2022-05-08 16:44:44 +0900

    com

--- quiz/TopViewController.swift
+++ quiz/TopViewController.swift
@@ -9,11 +9,9 @@
 import UIKit
 
 class TopViewController: UIViewController {
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
